cb83bcb38f76c079e81e5f79816b6b22236656fc
Tiernan <harristiernan90@gmail.com>
2026-09-22 14:55:48 +0100

    Update Program.cs

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.Xml.Serialization;
 
 void BankingApp()
@@ -6,51 +6,70 @@ void BankingApp()
     int choice = 0;
     double depositAmount;
     double accountBalance = 1000.00;
+    double withdrawAmount;
 
-    while (choice != 0)
+    while (choice != 4)
     {
         Console.WriteLine("Please choose an option from below.");
         Console.WriteLine("===================================");
-        Console.WriteLine("1 - Deposit or withdraw funds.");
-        Console.WriteLine("2 - View current account information.");
-        Console.WriteLine("0 - Exit");
-        Console.WriteLine("Please choose an option.");
+        Console.WriteLine("1 - Deposit funds.");
+        Console.WriteLine("2 - Withdraw funds.");
+        Console.WriteLine("3 - View current account information.");
+        Console.WriteLine("4 - Exit");
+        Console.WriteLine("Please choose an option.\n");
 
 
         choice = Convert.ToInt32(Console.ReadLine());
 
 
         {
-            if (choice == 1 || choice == 2)
+            if (choice == 1)
             {
-                if (choice == 1)
-                {
-                    Console.WriteLine("You have choosen to deposit or withdraw funds");
-                    Console.WriteLine("How much do you want to deposit?");
-                    Console.WriteLine("Enter the amount including pence using a decimal point");
+                Console.WriteLine("\nYou have choosen to deposit funds. \nHow much do you want to deposit?");
+                Console.WriteLine("\nEnter the amount including pence using a decimal point.");
 
-                    depositAmount = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine($"Your orginal account balance was {accountBalance} ");
+                depositAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"\nYour orginal account balance was {accountBalance}. ");
 
-                    accountBalance += depositAmount;
-                    Console.WriteLine($"Your new account balance is {accountBalance}");
+                accountBalance += depositAmount;
+                Console.WriteLine($"\nYour new account balance is {accountBalance}.");
+                Console.WriteLine();
 
-                }
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("\nYou have choosen to withdraw funds. \nHow much do you want to withdraw?");
+                Console.WriteLine("\nEnter the amount including pence using a decimal point.");
 
-                if (choice == 2)
-                {
-                    Console.WriteLine("You have chosen to view current account information");
-                    Console.WriteLine($"Your current account balance is {accountBalance}");
+                withdrawAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"\nYour orginal account balance was {accountBalance}. ");
+
+                accountBalance -= withdrawAmount;
+                Console.WriteLine($"Your new account balance is {accountBalance}.");
+                Console.WriteLine();
+
+            }
+
+            else if (choice == 3)
+            {
+                Console.WriteLine("\nYou have chosen to view current account information.");
+                Console.WriteLine($"\nYour current account balance is {accountBalance}.");
+                Console.WriteLine();
 
 
-                }
+
+            }
+
+            else if (choice == 4)
+            {
+                Console.WriteLine("\nThankyou, goodbye!");
             }
             else
             {
-                Console.WriteLine("Invalid choice, please choose 1 or 2.");
+                Console.WriteLine("Invalid choice, please choose 1, 2, 3 or 4.");
             }
         }
     }
 }
 
-BankingApp();
+    BankingApp();
